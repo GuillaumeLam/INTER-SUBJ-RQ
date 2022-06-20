8bbb6da60df27ae5d8ea5d7d587af3aac2da9a0e
Guillaume <guillaume.lam10100@gmail.com>
2022-06-19 23:06:27 -0400

    Added basic code to import ngois and subject wise split

--- INTER-SUBJ-CALIB/main.py
+++ INTER-SUBJ-CALIB/main.py
@@ -1,5 +1,28 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from util.subject_wise_split import subject_wise_split
+
+import sys
 import numpy as np
 
-X = np.load('X.npy', allow_pickle=True)
+ngois = np.load('../dataset/nGoIS_dataset.npy', allow_pickle=True)
 
-print(X)
+x_train, y_train, x_test, y_test, p_train, p_test = subject_wise_split(ngois[:,1],ngois[:,2], participant=ngois[:,0], subject_wise=True,split=0.1,seed=42)
+print('Successful subject wise split!')
+print('p_train shape:')
+print(p_train.shape)
+print('p_test shape:')
+print(p_test.shape)
+
+print('y_train shape:')
+print(y_train.shape)
+print('y_test shape:')
+print(y_test.shape)
+
+print('x_train shape:')
+print(x_train.shape)
+print('x_test shape:')
+print(x_test.shape)
