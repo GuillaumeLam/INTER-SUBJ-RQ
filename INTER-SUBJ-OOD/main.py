b7bdfb4e491d90116d210fdb86119408c45d537e
Guillaume <guillaume.lam10100@gmail.com>
2022-06-19 23:12:30 -0400

    Copied other rq main

--- INTER-SUBJ-OOD/main.py
+++ INTER-SUBJ-OOD/main.py
@@ -1,5 +1,13 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+from util.subject_wise_split import subject_wise_split
+
+import sys
 import numpy as np
 
-X = np.load('X.npy', allow_pickle=True)
+ngois = np.load('../dataset/nGoIS_dataset.npy', allow_pickle=True)
 
-print(X)
+x_train, y_train, x_test, y_test, p_train, p_test = subject_wise_split(ngois[:,1],ngois[:,2], participant=ngois[:,0], subject_wise=True,split=0.1,seed=42)
